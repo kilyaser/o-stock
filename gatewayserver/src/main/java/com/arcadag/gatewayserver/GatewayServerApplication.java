ae1e6b3a7f539f59ffe2396e3b16ac12a4628ab7
arcadag <piknik0711@yandex.ru>
2023-07-16 23:32:10 +0300

    added gateway configure

--- gatewayserver/src/main/java/com/arcadag/gatewayserver/GatewayServerApplication.java
+++ gatewayserver/src/main/java/com/arcadag/gatewayserver/GatewayServerApplication.java
@@ -2,12 +2,14 @@ package com.arcadag.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class GatewayserverApplication {
+@EnableEurekaClient
+public class GatewayServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayserverApplication.class, args);
+		SpringApplication.run(GatewayServerApplication.class, args);
 	}
 
 }
