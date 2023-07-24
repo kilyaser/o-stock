72f484875f6510b14c315f4729eed01a9f3f7587
arcadag <piknik0711@yandex.ru>
2023-07-24 14:33:25 +0300

    refactoring code

--- licensing-service/src/main/java/com/arcadag/license/config/AppConfig.java
+++ licensing-service/src/main/java/com/arcadag/license/config/AppConfig.java
@@ -1,14 +1,10 @@
 package com.arcadag.license.config;
 
 import com.arcadag.license.utils.UserContextInterceptor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -18,10 +14,8 @@ import java.util.List;
 import java.util.Locale;
 
 @Configuration
-@RequiredArgsConstructor
 public class AppConfig {
 
-    public final ServiceConfig serviceConfig;
     @Bean
     public SessionLocaleResolver localResolver() {
         SessionLocaleResolver localeResolver = new SessionLocaleResolver();
@@ -36,9 +30,10 @@ public class AppConfig {
         messageSource.setBasename("messages");
         return messageSource;
     }
+
     @LoadBalanced
     @Bean
-    public RestTemplate getRestTemplate(){
+    public RestTemplate getRestTemplate() {
         RestTemplate restTemplate = new RestTemplate();
         List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
         if (interceptors == null) {
@@ -49,17 +44,5 @@ public class AppConfig {
         }
         return restTemplate;
     }
-    @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
-        String hostname = serviceConfig.getRedisServer();
-        int port = Integer.parseInt(serviceConfig.getRedisPort());
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(hostname, port);
-        return  new JedisConnectionFactory(redisStandaloneConfiguration);
-    }
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        return template;
-    }
+
 }
